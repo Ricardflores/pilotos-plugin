940ca48214059ab989f3762eaaf8e785c4b2cbc6
ricardo <ricardo.flores@devitm.com>
2024-12-19 10:04:20 -0600

    clases de pilotos

--- admin/custom_fields/karting-fields.php
+++ admin/custom_fields/karting-fields.php
@@ -160,3 +160,63 @@ if (!function_exists('karting_redes')){
     add_action( 'cmb2_init', 'karting_redes' );
 }
 
+if (!function_exists('karting_selector')){
+
+
+    function karting_selector() {
+
+        // Registra un nuevo metabox
+        $prefix = 'selector_'; // Prefijo para los IDs de los campos
+
+        $cmb = new_cmb2_box( array(
+            'id'            => $prefix . 'metabox',
+            'title'          => __( 'Selector_piloto', 'http://primerapagina.local/' ),
+            'object_types'   => array( 'karting' ), // Cambia 'product' por tu tipo de publicación
+            'context'        => 'normal',
+            'priority'      => 'high',
+            'show_names'     => true,
+        ) );
+        $cmb->add_field( array(
+            'name'      	=> __( 'Piloto', 'cmb2' ),
+            'id'        	=>'yourprefix_demo_ajax',
+            'type'      	=> 'post_search_ajax',
+            'desc'			=> __( '(Start typing post title)', 'cmb2' ),
+            // Optional :
+            'limit'      	=> 10, 		// Limit selection to X items only (default 1)
+            'sortable' 	 	=> true, 	// Allow selected items to be sortable (default false)
+            'query_args'	=> array(
+                'post_type'			=> array( 'piloto' ),
+                'post_status'		=> array( 'publish' ),
+                'posts_per_page'	=> -1
+            )
+        ) );
+    }
+    add_action( 'cmb2_init', 'karting_selector' );
+}
+
+if (!function_exists('descripcion_karting')){
+
+    function descripcion_karting() {
+
+        // Registra un nuevo metabox
+        $prefix = 'campo2_'; // Prefijo para los IDs de los campos
+
+        $cmb = new_cmb2_box( array(
+            'id'            => $prefix . 'metabox',
+            'title'          => __( 'descripcion_karting', 'http://primerapagina.local/' ),
+            'object_types'   => array( 'karting' ), // Cambia 'product' por tu tipo de publicación
+            'context'        => 'normal',
+            'priority'      => 'high',
+            'show_names'     => true,
+        ) );
+        
+            // Agrega campos al metabox
+            $cmb->add_field( array(
+                'name'       => __( 'Descripcion karting', 'http://primerapagina.local/' ),
+                'id'         => $prefix . 'descripcion_karting',
+                'type'       => 'text',
+            ) );
+
+    }
+    add_action( 'cmb2_init', 'descripcion_karting' );
+}
